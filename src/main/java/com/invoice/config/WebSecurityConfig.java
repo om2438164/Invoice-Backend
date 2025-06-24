55fa78553f868d8ea9dcf323193ab86f40ce05bc
ompatil <om2438164@gmail.com>
2025-06-24 15:06:47 +0530

    added

--- src/main/java/com/invoice/config/WebSecurityConfig.java
+++ src/main/java/com/invoice/config/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://invoice-xi-bice.vercel.app"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
